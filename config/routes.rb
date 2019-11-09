a985cc7e3ec73174c4627dadc07e0cb00f471ec1
Alan Krajina <alankrajina88@gmail.com>
2019-11-09 13:15:51 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  resources :contacts
-  resources :comments
-  resources :blogs
-  resources :projects
-  resources :educations
-  resources :experiences
-  resources :abouts
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/signup", to: "api/v1/users#create"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+
+  namespace :api do
+    namespace :v1 do
+      resources :contacts
+      resources :comments
+      resources :blogs
+      resources :projects
+      resources :educations
+      resources :experiences
+      resources :abouts
+      resources :users
+    end
+  end
 end
