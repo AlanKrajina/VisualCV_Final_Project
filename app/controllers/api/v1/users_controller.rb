0e78767de73a2177a9fc06c81902c4df788c8b05
Alan Krajina <alankrajina88@gmail.com>
2019-11-15 18:07:33 +0100

    added wrap_parameters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  wrap_parameters :user, include: [:username, :password] # resolves password nill issue
+
 
   # GET /users
   def index
@@ -18,7 +20,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
+if @user.save
       session[:user_id] = @user.id
       render json: UserSerializer.new(@user), status: :created
     else
@@ -51,6 +53,8 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password_digest)
+      params.require(:user).permit(:username, :password)
+      #params.require(:user).permit!
+
     end
 end
