78c5eb70f03614510f7524a7af6dffc590fe0aa4
Alan Krajina <alankrajina88@gmail.com>
2020-01-20 15:58:48 +0100

    cors update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://visualcv.herokuapp.com/', 'http://visualcv.herokuapp.com/', 'https://visualcvreact.herokuapp.com/', 'http://visualcvreact.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
