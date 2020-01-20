6b68cef37f54530d899266b113d109394bf8ee11
Alan Krajina <alankrajina88@gmail.com>
2020-01-20 19:23:18 +0100

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'https://visualcv.herokuapp.com'
   
-      resource 'https://visualcv.herokuapp.com',
+      resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
