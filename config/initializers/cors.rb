e17a9e08c504c00bbe7a9a8edb7d5306d4ae1062
Alan Krajina <alankrajina88@gmail.com>
2020-01-20 15:19:24 +0100

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
+      origins '*'
   
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
     end
   end
