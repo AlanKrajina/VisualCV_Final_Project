d1d03030acac4b2d86233b084920d48a064fea2f
Alan Krajina <alankrajina88@gmail.com>
2020-01-21 20:22:42 +0100

    cors update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
     include ::ActionController::Cookies
-    
+    before_filter :set_headers
+
      def current_user
        User.find_by(id: session[:user_id])
      end
@@ -8,5 +9,21 @@ class ApplicationController < ActionController::API
      def logged_in?
        !!current_user
      end
+
+    private
+  
+    def set_headers
+      if request.headers["HTTP_ORIGIN"]
+      # better way check origin
+      # if request.headers["HTTP_ORIGIN"] && /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+        headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+        headers['Access-Control-Expose-Headers'] = 'ETag'
+        headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+        headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+        headers['Access-Control-Max-Age'] = '86400'
+        headers['Access-Control-Allow-Credentials'] = 'true'
+      end
+    end
+
    
    end
