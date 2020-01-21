1f8a5ece81f3c78ab8cc0be65e840823c3aa3965
Alan Krajina <alankrajina88@gmail.com>
2020-01-21 20:30:50 +0100

    cors update

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module VisualCVFinalProject
      config.middleware.insert_before 0, Rack::Cors do
        allow do
          origins 'https://visualcv.herokuapp.com'
-         resource '*', headers: :origin, methods: [:get, :patch, :put, :delete, :post, :options]
+         resource 'https://visualcv.herokuapp.com', headers: :any, methods: [:get, :patch, :put, :delete, :post, :options]
        end
      end    
 
